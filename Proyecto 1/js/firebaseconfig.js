2cbbcfd6aa0d727a2f24d44698c58e0c6e6691dc
Emilio <emilio.buendia@saei.mx>
2026-08-07 07:12:12 -0600

    La buena

--- Proyecto 1/js/firebaseconfig.js
+++ Proyecto 1/js/firebaseconfig.js
@@ -1,5 +1,11 @@
 const firebaseConfig = {
-  //Usar propio config
+  apiKey: "AIzaSyAG7qXZMxlPY-zPrfiinQYNJq3_TWIfRWg",
+  authDomain: "initialtestproject-2f3b4.firebaseapp.com",
+  databaseURL: "https://initialtestproject-2f3b4-default-rtdb.firebaseio.com",
+  projectId: "initialtestproject-2f3b4",
+  storageBucket: "initialtestproject-2f3b4.firebasestorage.app",
+  messagingSenderId: "717731776435",
+  appId: "1:717731776435:web:8a673cbc6454c0d7392dc6"
 };
 
 firebase.initializeApp(firebaseConfig);
